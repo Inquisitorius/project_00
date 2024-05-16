530d4ce0f36fe12b5376bd63f80c7ffc1f98b955
wjrls <wjrls@naver.com>
2024-05-16 10:50:29 +0900

    SceneChanger 작성 및 MainFrame 수정작업

--- src/projectPackage/MainFrame.java
+++ src/projectPackage/MainFrame.java
@@ -5,20 +5,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.GridLayout;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import java.awt.FlowLayout;
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
+
 import java.awt.Component;	
 
 import javax.swing.BoxLayout;
@@ -28,11 +18,11 @@ public class MainFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private int index;
 	private DBRequester dbRequester;
+	private SceneChanger sceneChanger;
 	
 	private JPanel MainPane;	
 	private JPanel HeadPanel;	
 	
-	private HashMap<MainFrame.PANELNAME, JPanel> panelMap;
 	private UserInfoVo userInfo;
 	private TicketVo selectedInfo;
 	
@@ -81,49 +71,8 @@ public class MainFrame extends JFrame {
 		
 		userInfo = null;
 		index = 0;		
-		panelMap = new HashMap<MainFrame.PANELNAME, JPanel>();		
 		
-		HeadPanel = new TopPanelTest(this);	
-		//생성 판넬 관리
-		//JPanel ContentPanel = new SeatSelectPage(this);
-		JPanel ContentPanel = new OhtanisPanel(this);
-		JPanel ContentPanel2 = new Admin_UserList(this);
-		JPanel ContentPanel3 = new Admin_MovieList(this);
-		JPanel ContentPanel4 = new Ticketreserve(this);
-	    JPanel ContentPanel5 = new AnCont2(this);
-	    JPanel ContentPanel6 = new AnCont3(this);
-	    JPanel ContentPanel7 = new AnCont4(this);
-	    JPanel ContentPanel8 = new Login_New(this);
-	    JPanel ContentPanel9 = new CreateId_1(this);
-	    JPanel ContentPanel10 = new CreateId_2(this);
-	    JPanel ContentPanel11 = new CreateId_3(this);
-	    JPanel ContentPanel12 = new BirdRichard(this);
-	    JPanel ContentPanel13 = new JoinAgree(this);
-	    JPanel ContentPanel14 = new MyPage_New(this);
-	    JPanel ContentPanel15 = new SeatSelectPage(this);
-	    JPanel ContentPanel16 = new JinsungInformation(this);
-	    JPanel ContentPanel17 = new JinsungPanelT(this);
-	    
-		
-		//생성된 판넬 Map 자료 구조에 넣기
-		panelMap.put(MainFrame.PANELNAME.MAIN, ContentPanel);
-		panelMap.put(MainFrame.PANELNAME.PAGE0, ContentPanel2);
-		panelMap.put(MainFrame.PANELNAME.PAGE1, ContentPanel3);
-	    panelMap.put(MainFrame.PANELNAME.TICKETING, ContentPanel4);
-	    panelMap.put(MainFrame.PANELNAME.SELECT, ContentPanel5);
-	    panelMap.put(MainFrame.PANELNAME.PAYMENT, ContentPanel6);
-	    panelMap.put(MainFrame.PANELNAME.TICKET, ContentPanel7);
-	    panelMap.put(MainFrame.PANELNAME.LOGIN, ContentPanel8);
-	    panelMap.put(MainFrame.PANELNAME.JOIN1, ContentPanel9);
-	    panelMap.put(MainFrame.PANELNAME.JOIN2, ContentPanel10);
-	    panelMap.put(MainFrame.PANELNAME.JOIN3, ContentPanel11);
-	    panelMap.put(MainFrame.PANELNAME.BIRD, ContentPanel12);
-	    panelMap.put(MainFrame.PANELNAME.JOIN_AGREE, ContentPanel13);
-	    panelMap.put(MainFrame.PANELNAME.MYPAGE, ContentPanel14);
-	    panelMap.put(MainFrame.PANELNAME.SEATSELECT, ContentPanel15);
-	    panelMap.put(MainFrame.PANELNAME.TICKETINFO, ContentPanel16);
-	    panelMap.put(MainFrame.PANELNAME.TICKETCANCLE, ContentPanel17);
-	    
+		//HeadPanel = new TopPanelTest(this);	
 		
 		//Main판넬 설정
 		setBounds(100, 100, 1280, 800);
@@ -133,29 +82,12 @@ public class MainFrame extends JFrame {
 		setContentPane(MainPane);
 		MainPane.setLayout(new BoxLayout(MainPane, BoxLayout.Y_AXIS));
 		
-		//메인 페이지 초기화 코드
-		ContentPanel.setVisible(true);
-		//ContentPanel3.setVisible(true);
-		
-		//Main 판넬에 하위 판넬 add 처리
-		this.getContentPane().add(HeadPanel);
-		this.getContentPane().add(ContentPanel);
-		this.getContentPane().add(ContentPanel2);
-		this.getContentPane().add(ContentPanel3);
-		this.getContentPane().add(ContentPanel4);
-	    this.getContentPane().add(ContentPanel5);
-	    this.getContentPane().add(ContentPanel6);
-	    this.getContentPane().add(ContentPanel7);
-	    this.getContentPane().add(ContentPanel8);   //LOGIN
-	    this.getContentPane().add(ContentPanel9);   //JOIN1
-	    this.getContentPane().add(ContentPanel10);   //JOIN2
-	    this.getContentPane().add(ContentPanel11);   //JOIN3	
-	    this.getContentPane().add(ContentPanel12); 	 // bird
-	    this.getContentPane().add(ContentPanel13);
-	    this.getContentPane().add(ContentPanel14);
-	    this.getContentPane().add(ContentPanel15);
-	    this.getContentPane().add(ContentPanel16);
-	    this.getContentPane().add(ContentPanel17);
+		sceneChanger = new SceneChanger(this);
+	}
+	
+	public void PanelAdd(Component comp)
+	{
+		 this.getContentPane().add(comp);
 	}
 	
 	@Override
@@ -186,112 +118,51 @@ public class MainFrame extends JFrame {
 	
 	public void PageChange(PANELNAME name)
 	{		
-		 
-		Iterator<PANELNAME> keys = panelMap.keySet().iterator();
-		while (keys.hasNext()) {
-			PANELNAME key = keys.next();
-			panelMap.get(key).setVisible(false);
-		}
+		this.sceneChanger.PageChange(name);
 		
-		
-		switch (name) {
-		case MAIN: 						
-			Pagechange_init(panelMap.get(MainFrame.PANELNAME.MAIN), true);
-			break;
-		case PAGE0:
-			Pagechange_init(panelMap.get(MainFrame.PANELNAME.PAGE0), true);
-			break;
-		case PAGE1:
-			Pagechange_init(panelMap.get(MainFrame.PANELNAME.PAGE1), true);			
-			break;	
-		case PAGE2:
-			panelMap.get(MainFrame.PANELNAME.PAGE2).setVisible(true);
-			break;	
-	     case TICKETING:
-	    	((Ticketreserve)panelMap.get(MainFrame.PANELNAME.TICKETING)).PageOpenInit();
-	         panelMap.get(MainFrame.PANELNAME.TICKETING).setVisible(true);
-	         break;   
-	      case SELECT:
-	         panelMap.get(MainFrame.PANELNAME.SELECT).setVisible(true);
-	         break;   
-	      case PAYMENT:
-	         panelMap.get(MainFrame.PANELNAME.PAYMENT).setVisible(true);
-	         break;   
-	      case TICKET:
-	         panelMap.get(MainFrame.PANELNAME.TICKET).setVisible(true);
-	         break;
-	      case LOGIN:
-	    	 ((Login_New)panelMap.get(MainFrame.PANELNAME.LOGIN)).PageOpen_Init();
-	         panelMap.get(MainFrame.PANELNAME.LOGIN).setVisible(true);
-	         break;
-	      case JOIN1:
-	         panelMap.get(MainFrame.PANELNAME.JOIN1).setVisible(true);
-	         break;
-	      case JOIN2:
-	         panelMap.get(MainFrame.PANELNAME.JOIN2).setVisible(true);
-	         break;
-	      case JOIN3:
-	         panelMap.get(MainFrame.PANELNAME.JOIN3).setVisible(true);
-	         break;
-	      case BIRD:
-	    	 panelMap.get(MainFrame.PANELNAME.BIRD).setVisible(true);
-		     break;
-	      case JOIN_AGREE:
-	    	  panelMap.get(MainFrame.PANELNAME.JOIN_AGREE).setVisible(true);
-	    	  break;      
-	      case MYPAGE:	    	  
-	    	 ((MyPage_New)panelMap.get(MainFrame.PANELNAME.MYPAGE)).MypageInit();
-	    	 panelMap.get(MainFrame.PANELNAME.MYPAGE).setVisible(true);
-	    	 break;
-	      case SEATSELECT:
-	    	  ((SeatSelectPage)panelMap.get(MainFrame.PANELNAME.SEATSELECT)).PageInit();
-	    	  panelMap.get(MainFrame.PANELNAME.SEATSELECT).setVisible(true);
-	    	  break;
-	      case TICKETINFO:
-	    	  ((JinsungInformation)panelMap.get(MainFrame.PANELNAME.TICKETINFO)).PageInit();
-	    	  panelMap.get(MainFrame.PANELNAME.TICKETINFO).setVisible(true);
-	    	  break;  
-	      case TICKETCANCLE:
-	    	  ((JinsungPanelT)panelMap.get(MainFrame.PANELNAME.TICKETCANCLE)).PageInit();
-	    	  panelMap.get(MainFrame.PANELNAME.TICKETCANCLE).setVisible(true);
-	    	  break;	    	
-		}
 	}
 	
 	public void Set_selectedMovieName_TicketReserve(String movieName)
 	{
-		((Ticketreserve)panelMap.get(MainFrame.PANELNAME.TICKETING)).Set_selectedMovieName(movieName);
+		this.sceneChanger.Set_selectedMovieName_TicketReserve(movieName);
+		//((Ticketreserve)panelMap.get(MainFrame.PANELNAME.TICKETING)).Set_selectedMovieName(movieName);
 	}
 	
 	public void Set_TicketInfo_Cancle(TicketVo ticketInfo)
 	{
-		((JinsungPanelT)panelMap.get(MainFrame.PANELNAME.TICKETCANCLE)).Set_TicketInfo_Cancle(ticketInfo);
+		this.sceneChanger.Set_TicketInfo_Cancle(ticketInfo);
+		//((JinsungPanelT)panelMap.get(MainFrame.PANELNAME.TICKETCANCLE)).Set_TicketInfo_Cancle(ticketInfo);
 	}
 	
 	public void Set_TicketCanclePage_TicketNo(int ticketNo)
 	{
-		((JinsungPanelT)panelMap.get(MainFrame.PANELNAME.TICKETCANCLE)).Set_TicketNo(ticketNo);
+		this.sceneChanger.Set_TicketCanclePage_TicketNo(ticketNo);
+		//((JinsungPanelT)panelMap.get(MainFrame.PANELNAME.TICKETCANCLE)).Set_TicketNo(ticketNo);
 	}
 	
 	public void Set_TicketRserveData_forSeatSelect(String _moviName, String _movieHouseName, String _localName, String _timeData)
 	{
-		((SeatSelectPage)panelMap.get(MainFrame.PANELNAME.SEATSELECT)).Set_TicketRserveData(_moviName, _movieHouseName, _localName, _timeData);
+		this.sceneChanger.Set_TicketRserveData_forSeatSelect(_moviName, _movieHouseName, _localName, _timeData);
+		//((SeatSelectPage)panelMap.get(MainFrame.PANELNAME.SEATSELECT)).Set_TicketRserveData(_moviName, _movieHouseName, _localName, _timeData);
 	}
 	
 	public JPanel Get_Panel_Main()
 	{		
-		return panelMap.get(MainFrame.PANELNAME.MAIN);
+		return this.sceneChanger.Get_Panel_Main();
+		//return panelMap.get(MainFrame.PANELNAME.MAIN);
 	}
 	
 	public void Set_UserInfo_forLogout()
 	{
 		this.userInfo = null;
-		((TopPanelTest)HeadPanel).Logout_Init();
+		this.sceneChanger.Set_UserInfo_forLogout();
+		//((TopPanelTest)HeadPanel).Logout_Init();
 	}
 	
 	public void Login_Progress()
 	{
-		((TopPanelTest)HeadPanel).Login_Init();
+		this.sceneChanger.Login_Progress();
+		//((TopPanelTest)HeadPanel).Login_Init();
 	}
 	
 	public UserInfoVo Get_UserInfo()
---
+++ src/projectPackage/SceneChanger.java
@@ -0,0 +1,187 @@
+package projectPackage;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+import javax.swing.JPanel;
+
+import projectPackage.MainFrame.PANELNAME;
+
+public class SceneChanger {
+
+	private MainFrame mainFrame;
+	private HashMap<MainFrame.PANELNAME, JPanel> panelMap;
+	
+	private JPanel HeadPanel;
+	private PageChangeMessage messageObj;
+	
+	public SceneChanger(MainFrame mainFrame) {
+		// TODO Auto-generated constructor stub
+		this.mainFrame = mainFrame;
+		this.messageObj = new PageChangeMessage(mainFrame);
+		
+		HeadPanel = new TopPanelTest(this.mainFrame);	
+		HeadPanel.setVisible(true);
+		
+		panelMap = new HashMap<MainFrame.PANELNAME, JPanel>();
+		
+		JPanel ContentPanel = new OhtanisPanel(this.mainFrame);
+		JPanel ContentPanel2 = new Admin_UserList(this.mainFrame);
+		JPanel ContentPanel3 = new Admin_MovieList(this.mainFrame);
+		JPanel ContentPanel4 = new Ticketreserve(this.mainFrame);	
+	    JPanel ContentPanel8 = new Login_New(this.mainFrame);
+	    JPanel ContentPanel12 = new BirdRichard(this.mainFrame);
+	    JPanel ContentPanel13 = new JoinAgree(this.mainFrame);
+	    JPanel ContentPanel14 = new MyPage_New(this.mainFrame);
+	    JPanel ContentPanel15 = new SeatSelectPage(this.mainFrame);
+	    JPanel ContentPanel16 = new JinsungInformation(this.mainFrame);
+	    JPanel ContentPanel17 = new JinsungPanelT(this.mainFrame);
+	    
+	    panelMap.put(MainFrame.PANELNAME.MAIN, ContentPanel);
+		panelMap.put(MainFrame.PANELNAME.PAGE0, ContentPanel2);
+		panelMap.put(MainFrame.PANELNAME.PAGE1, ContentPanel3);
+	    panelMap.put(MainFrame.PANELNAME.TICKETING, ContentPanel4);
+	    panelMap.put(MainFrame.PANELNAME.LOGIN, ContentPanel8);
+	    panelMap.put(MainFrame.PANELNAME.BIRD, ContentPanel12);
+	    panelMap.put(MainFrame.PANELNAME.JOIN_AGREE, ContentPanel13);
+	    panelMap.put(MainFrame.PANELNAME.MYPAGE, ContentPanel14);
+	    panelMap.put(MainFrame.PANELNAME.SEATSELECT, ContentPanel15);
+	    panelMap.put(MainFrame.PANELNAME.TICKETINFO, ContentPanel16);
+	    panelMap.put(MainFrame.PANELNAME.TICKETCANCLE, ContentPanel17);
+	    
+	    //Main Page Visible
+	    ContentPanel.setVisible(true);
+	    
+	    this.mainFrame.PanelAdd(HeadPanel);
+	    this.mainFrame.PanelAdd(ContentPanel);
+	    this.mainFrame.PanelAdd(ContentPanel2);
+	    this.mainFrame.PanelAdd(ContentPanel3);
+	    this.mainFrame.PanelAdd(ContentPanel4);
+	    this.mainFrame.PanelAdd(ContentPanel8);
+	    this.mainFrame.PanelAdd(ContentPanel12);
+	    this.mainFrame.PanelAdd(ContentPanel13);
+	    this.mainFrame.PanelAdd(ContentPanel14);
+	    this.mainFrame.PanelAdd(ContentPanel15);
+	    this.mainFrame.PanelAdd(ContentPanel16);
+	    this.mainFrame.PanelAdd(ContentPanel17);
+	    
+	}
+	
+	public void PopupVisible(String value, MainFrame.PANELNAME PAGETYPE)
+	{
+		this.messageObj.PopupVisible(value, PAGETYPE);
+	}
+
+	public void setMessageObj(PageChangeMessage messageObj) {
+		this.messageObj = messageObj;
+	}
+	
+	public void Set_selectedMovieName_TicketReserve(String movieName)
+	{
+		((Ticketreserve)panelMap.get(MainFrame.PANELNAME.TICKETING)).Set_selectedMovieName(movieName);
+	}
+	
+	public void Set_TicketInfo_Cancle(TicketVo ticketInfo)
+	{
+		((JinsungPanelT)panelMap.get(MainFrame.PANELNAME.TICKETCANCLE)).Set_TicketInfo_Cancle(ticketInfo);
+	}
+	
+	public void Set_TicketCanclePage_TicketNo(int ticketNo)
+	{
+		((JinsungPanelT)panelMap.get(MainFrame.PANELNAME.TICKETCANCLE)).Set_TicketNo(ticketNo);
+	}
+	
+	public void Set_TicketRserveData_forSeatSelect(String _moviName, String _movieHouseName, String _localName, String _timeData)
+	{
+		((SeatSelectPage)panelMap.get(MainFrame.PANELNAME.SEATSELECT)).Set_TicketRserveData(_moviName, _movieHouseName, _localName, _timeData);
+	}
+	
+	public JPanel Get_Panel_Main()
+	{		
+		return panelMap.get(MainFrame.PANELNAME.MAIN);
+	}
+	
+	public void Set_UserInfo_forLogout()
+	{
+		((TopPanelTest)HeadPanel).Logout_Init();
+	}
+	
+	public void Login_Progress()
+	{
+		((TopPanelTest)HeadPanel).Login_Init();
+	}
+	
+	
+	
+	public void PageChange(PANELNAME name)
+	{	 
+		Iterator<PANELNAME> keys = panelMap.keySet().iterator();
+		while (keys.hasNext()) {
+			PANELNAME key = keys.next();
+			panelMap.get(key).setVisible(false);
+		}		
+		
+		switch (name) {
+		case MAIN: 	
+			panelMap.get(MainFrame.PANELNAME.MAIN).setVisible(true);
+			break;
+		case PAGE0:
+			panelMap.get(MainFrame.PANELNAME.PAGE0).setVisible(true);
+			break;
+		case PAGE1:
+			panelMap.get(MainFrame.PANELNAME.PAGE1).setVisible(true);
+			break;	
+		case PAGE2:
+			panelMap.get(MainFrame.PANELNAME.PAGE2).setVisible(true);
+			break;	
+	     case TICKETING:
+	    	((Ticketreserve)panelMap.get(MainFrame.PANELNAME.TICKETING)).PageOpenInit();
+	         panelMap.get(MainFrame.PANELNAME.TICKETING).setVisible(true);
+	         break;   
+	      case SELECT:
+	         panelMap.get(MainFrame.PANELNAME.SELECT).setVisible(true);
+	         break;   
+	      case PAYMENT:
+	         panelMap.get(MainFrame.PANELNAME.PAYMENT).setVisible(true);
+	         break;   
+	      case TICKET:
+	         panelMap.get(MainFrame.PANELNAME.TICKET).setVisible(true);
+	         break;
+	      case LOGIN:
+	    	 ((Login_New)panelMap.get(MainFrame.PANELNAME.LOGIN)).PageOpen_Init();
+	         panelMap.get(MainFrame.PANELNAME.LOGIN).setVisible(true);
+	         break;
+	      case JOIN1:
+	         panelMap.get(MainFrame.PANELNAME.JOIN1).setVisible(true);
+	         break;
+	      case JOIN2:
+	         panelMap.get(MainFrame.PANELNAME.JOIN2).setVisible(true);
+	         break;
+	      case JOIN3:
+	         panelMap.get(MainFrame.PANELNAME.JOIN3).setVisible(true);
+	         break;
+	      case BIRD:
+	    	 panelMap.get(MainFrame.PANELNAME.BIRD).setVisible(true);
+		     break;
+	      case JOIN_AGREE:
+	    	  panelMap.get(MainFrame.PANELNAME.JOIN_AGREE).setVisible(true);
+	    	  break;      
+	      case MYPAGE:	    	  
+	    	 ((MyPage_New)panelMap.get(MainFrame.PANELNAME.MYPAGE)).MypageInit();
+	    	 panelMap.get(MainFrame.PANELNAME.MYPAGE).setVisible(true);
+	    	 break;
+	      case SEATSELECT:
+	    	  ((SeatSelectPage)panelMap.get(MainFrame.PANELNAME.SEATSELECT)).PageInit();
+	    	  panelMap.get(MainFrame.PANELNAME.SEATSELECT).setVisible(true);
+	    	  break;
+	      case TICKETINFO:
+	    	  ((JinsungInformation)panelMap.get(MainFrame.PANELNAME.TICKETINFO)).PageInit();
+	    	  panelMap.get(MainFrame.PANELNAME.TICKETINFO).setVisible(true);
+	    	  break;  
+	      case TICKETCANCLE:
+	    	  ((JinsungPanelT)panelMap.get(MainFrame.PANELNAME.TICKETCANCLE)).PageInit();
+	    	  panelMap.get(MainFrame.PANELNAME.TICKETCANCLE).setVisible(true);
+	    	  break;	    	
+		}
+	}
+}
